5d275b60070c7a7ae56a1546cae7876bc3762169
Mahesh@Webdev <maheshbadampudi31@gmail.com>
2024-03-16 23:32:42 +0530

    class1

---
+++ primitiveDT.java
@@ -0,0 +1,28 @@
+public class primitiveDT {
+
+    public static void main(String[] args) {
+
+        int x=5;
+        int y=7;
+        System.out.println("x is: "+x);
+        System.out.println("y is: "+y);
+
+        byte b=10;
+        System.out.println("b is: "+b);
+
+        byte b2=(byte)129;
+        System.out.println("b2 is :"+b2);
+
+        byte b3=(byte)128;
+        System.out.println("b3 is :"+b3);
+
+        byte b4=(byte)256;
+        System.out.println("b4 is :"+b4);
+
+
+
+        int i=b;
+        System.out.println("i is :"+i);
+
+    }
+}
